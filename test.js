f65cc3eb21b2881de4bde5cbadbf4d168d3fc8b4
andrewgosselin-nt <andrew.gosselin@ntdealerservices.com>
2021-02-09 13:44:15 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+    console.log( "ready!!!!" );
+    $('#userChangeSelect').on('change', function (element) {
+        $.ajax({
+            url: "/apps/ntsso/ssobridge/changeUser/" + this.value,
+            type: 'GET',
+            contentType: 'application/json'
+        }).done(function (users) {
+            window.location.href="/apps/ntsso/ssobridge/login";
+        }).fail(function (response, code) {
+            console.log("error", response);
+        });
+    });
+});
